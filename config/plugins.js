f4a8567f06c3dceefa34f77a805e380c8108990b
Javier Hertfelder <javier@fxstreet.com>
2023-07-10 11:17:10 +0200

    Update plugin.js with Netlify access token

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => ({
     "netlify-deployments": {
       enabled: true,
+      config: {
+        accessToken: "5rpM0ABdHt-IntztnKQFgIcPpWUld-CYAbBbw6eoXo4",
+        sites: [
+          {
+            name: 'indigostudiostrapi',
+            id: "1122a876-7ad5-4864-a68a-b80bf199dd1c",
+            buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            branch: 'master' // optional
+          }
+        ]
+      },
     },
   });
