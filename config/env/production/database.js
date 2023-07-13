5ec50822cc8d99b1be575a3d84403a5bd8041718
Javier Hertfelder <javier@fxstreet.com>
2023-07-13 12:28:27 +0200

    fixing env variable DATABASE_URL

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env(" postgres://ydjdwolsefzoym:1b9a49c8915c0b1fde1425fa6276445dc2273d184e2a2375f265c9cf3e41acfd@ec2-34-240-122-104.eu-west-1.compute.amazonaws.com:5432/d7vjfbiqfpstd7"));
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   
   return {
     connection: {
